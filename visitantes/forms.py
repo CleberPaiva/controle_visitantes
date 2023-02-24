19a2e6132eae2947cb4ea984716597f3463896eb
Cleber Paiva <58832413+CleberPaiva@users.noreply.github.com>
2023-02-24 15:19:08 -0300

    Criação módulo "informações_visitantes"

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -8,3 +8,19 @@ class VisitanteForm(forms.ModelForm):
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo"
         ]
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para o registro!"
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para o registro!"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatório para o registro!",
+                "invalid": "Por favor, informe um formato válido. (dd/mm/aaaa)"
+            },
+            "numero_casa": {
+                "required": "O número da casa é obrigatório para o registro!"
+            }          
+        }
+        
