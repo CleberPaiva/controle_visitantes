f87ab2df191a396ccefed045fa88c8777421d03c
Cleber Paiva <58832413+CleberPaiva@users.noreply.github.com>
2023-02-25 20:50:00 -0300

    Criando o path da url do login

--- controle_visitantes/urls.py
+++ controle_visitantes/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib.auth import views as auth_views
+
 from apps.dashboard.views import index
 
 from apps.visitantes.views import (
@@ -9,6 +11,7 @@ from apps.visitantes.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="login.html"), name="login"),   
     path("", index, name="index"),
     path("registrar-visitante/", registrar_visitante, name="registrar_visitante" ),
     path("visitantes/<int:id>/", informacoes_visitante, name="informacoes_visitante"),
