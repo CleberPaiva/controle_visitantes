c7d57d423601eaa17174d7a312ca2ee12fc313c0
Cleber Paiva <58832413+CleberPaiva@users.noreply.github.com>
2023-02-24 21:46:56 -0300

    Criação da pasta "apps"

--- controle_visitantes/settings.py
+++ controle_visitantes/settings.py
@@ -1,13 +1,10 @@
 
 import os
+import sys
+
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-5g5=301n9d=!b&^a&y8l8%kvr73(p9!gf4$j8gndplb-@pjad#'
@@ -16,6 +13,10 @@ SECRET_KEY = 'django-insecure-5g5=301n9d=!b&^a&y8l8%kvr73(p9!gf4$j8gndplb-@pjad#
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+sys.path.append(
+    os.path.join(BASE_DIR, "apps")
+)
 
 
 # Application definition
